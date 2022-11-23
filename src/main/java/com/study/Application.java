75abc8706d8d300cf38c04649e02c25faa68b17c
wangxiaoming <1940694270@qq.com>
2022-11-23 09:14:13 +0800

    build project

--- src/main/java/com/study/Application.java
+++ src/main/java/com/study/Application.java
@@ -3,6 +3,9 @@ package com.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author 王贤
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
